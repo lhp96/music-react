0028d10766edbe9d028794d3f01767c53509d100
luhuipeng <973880264@qq.com>
2022-11-19 12:28:15 +0800

    feat: add iconfont

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Music-React
 
 - 网易云音乐-React
+
+## 技术栈
+
+- pnpm + Vite
+- React
+- styled-components
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { GlobalStyle } from "./style";
+import { useState } from "react";
+import { IconStyle } from "./assets/iconfont/iconfont";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1>Vite + React</h1>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      <i className="iconfont">&#xe62b;</i>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
